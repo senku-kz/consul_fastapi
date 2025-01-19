1067125ce88750b13e2998c9d9d1ef66a596cd32
Rustem Sankubayev <senku.kz@gmail.com>
2025-01-20 00:48:24 +0500

    ok

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     
     # Application settings
     APP_NAME: str = "fastapi-service"
-    DEBUG: bool = True
+    DEBUG: bool = False
     
     # Server settings
     HOST: str = "0.0.0.0"
@@ -22,7 +22,9 @@ class Settings(BaseSettings):
     
     class Config:
         env_file = ".env"
+        env_file_encoding = "utf-8"
         case_sensitive = True
+        extra = "ignore"  # Ignore any extra env variables
 
 @lru_cache
 def get_settings() -> Settings:
